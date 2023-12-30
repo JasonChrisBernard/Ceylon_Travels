d001a2d8737a74b95bb9d27f98a501ffd907d2af
Jason Bernard <113552490+JasonChrisBernard@users.noreply.github.com>
2023-12-30 20:14:07 +0530

    Delete Travel-web/formula.py

--- Travel-web/formula.py
+++
@@ -1,7 +0,0 @@
-Se = int(input("Enter the value for SE :"))
-P = int(input("Enter the value for P :"))
-Sp = int(input("Enter the value for S :"))
-
-VPP = Se * P /( Se * P + (1-Sp) * (1-P))
-
-print(VPP)
